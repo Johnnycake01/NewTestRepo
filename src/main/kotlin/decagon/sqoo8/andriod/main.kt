22cc7c587755a788b292bfa4bb03c5c4a3ca980b
Johnnycake01 <oyesinajohnson@gmail.com>
2021-07-02 09:36:02 -0700

    update code

--- src/main/kotlin/decagon/sqoo8/andriod/main.kt
+++ src/main/kotlin/decagon/sqoo8/andriod/main.kt
@@ -1,13 +1,20 @@
 import decagon.sqoo8.andriod.Aquarium
+import java.util.*
 
 fun main(args: Array<String>) {
     println("Hello World!")
 
-  //  buildAquarium("totoise")
-    var tortoise = Aquarium()
-    tortoise.height = Integer.valueOf(readLine())
-    print(tortoise.height)
+    println(buildAquarium("totoise"))
+//    val tortoise = Aquarium()
+//    tortoise.height = Integer.valueOf(readLine())
+//    print(tortoise.height)
 }
-//fun buildAquarium(name:String){
-//    var name = Aquarium()
-//}
+fun buildAquarium(name:String):String{
+    var name = Aquarium()
+    println("enter length")
+    name.length = Integer.valueOf(readLine())
+    println("enter height")
+    name.height = Integer.valueOf(readLine())
+    var arr = arrayOf(name.length,name.height)
+    return Arrays.toString(arr)
+}
