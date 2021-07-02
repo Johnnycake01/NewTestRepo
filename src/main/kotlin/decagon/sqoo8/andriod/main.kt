968ff2101b37118d26de65533f8354694622e9a2
Johnnycake01 <oyesinajohnson@gmail.com>
2021-07-02 03:01:40 -0700

    create object of class Aquarium and got the height property from user

--- src/main/kotlin/decagon/sqoo8/andriod/main.kt
+++ src/main/kotlin/decagon/sqoo8/andriod/main.kt
@@ -1,3 +1,13 @@
+import decagon.sqoo8.andriod.Aquarium
+
 fun main(args: Array<String>) {
     println("Hello World!")
+
+  //  buildAquarium("totoise")
+    var tortoise = Aquarium()
+    tortoise.height = Integer.valueOf(readLine())
+    print(tortoise.height)
 }
+//fun buildAquarium(name:String){
+//    var name = Aquarium()
+//}
